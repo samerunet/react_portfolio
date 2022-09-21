fa61e4d9541a20db19afb62e89dce5883c07b311
samerunet <samerunet@gmail.com>
2022-09-21 11:09:25 -0700

    adding styling

--- src/Context.js
+++ src/Context.js
@@ -5,19 +5,17 @@ export const themeContext = createContext();
 const initialState = { darkMode: false };
 
 const themeReducer = (state, action) => {
-	switch (action.type) {
-		case "toggle":
-			return { darkMode: !state.darkMode };
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case "toggle":
+        return { darkMode: !state.darkMode };
+      default:
+        return state;
+  }
 };
 
 export const ThemeProvider = (props) => {
-	const [state, dispatch] = useReducer(themeReducer, initialState);
-	return (
-		<themeContext.Provider value={{ state, dispatch }}>
-			{props.children}
-		</themeContext.Provider>
-	);
+  const [state, dispatch] = useReducer(themeReducer, initialState);
+  return (
+    <themeContext.Provider value={{state, dispatch}}>{props.children}</themeContext.Provider>
+  );
 };
