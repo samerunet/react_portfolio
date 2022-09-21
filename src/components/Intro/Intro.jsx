48172869e5b61c9acf93489557546cda54e69d78
samerunet <samerunet@gmail.com>
2022-09-21 12:29:45 -0700

    adding content

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -14,84 +14,90 @@ import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
 import { Link } from "react-scroll";
 const Intro = () => {
-  // Transition
-  const transition = { duration: 2, type: "spring" };
+	// Transition
+	const transition = { duration: 2, type: "spring" };
 
-  // context
-  const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
+	// context
+	const theme = useContext(themeContext);
+	const darkMode = theme.state.darkMode;
 
-  return (
-    <div className="Intro" id="Intro">
-      {/* left name side */}
-      <div className="i-left">
-        <div className="i-name">
-          {/* yahan change hy darkmode ka */}
-          <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
-          <span>Andrew Thomas</span>
-          <span>
-            Frontend Developer with high level of experience in web designing
-            and development, producting the Quality work
-          </span>
-        </div>
-        <Link to="contact" smooth={true} spy={true}>
-          <button className="button i-button">Hire me</button>
-        </Link>
-        {/* social icons */}
-        <div className="i-icons">
-          <img src={Github} alt="" />
-          <img src={LinkedIn} alt="" />
-          <img src={Instagram} alt="" />
-        </div>
-      </div>
-      {/* right image side */}
-      <div className="i-right">
-        <img src={Vector1} alt="" />
-        <img src={Vector2} alt="" />
-        <img src={boy} alt="" />
-        {/* animation */}
-        <motion.img
-          initial={{ left: "-36%" }}
-          whileInView={{ left: "-24%" }}
-          transition={transition}
-          src={glassesimoji}
-          alt=""
-        />
+	return (
+		<div className='Intro' id='Intro'>
+			{/* left name side */}
+			<div className='i-left'>
+				<div className='i-name'>
+					{/*mode change */}
+					<span style={{ color: darkMode ? "white" : "" }}>Hey! I Am</span>
+					<span>Sami Sipahi</span>
+					<span>
+						Frontend Developer with experience in web development and
+						development, producting the Quality work and
+						<br />
+						Quality code by applying the right Algorithm to produce the best
+						outcome
+					</span>
+				</div>
+				<Link to='contact' smooth={true} spy={true}>
+					<button className='button i-button'>Contact me</button>
+				</Link>
+				{/* social icons */}
+				<div className='i-icons'>
+					<img src={Github} alt='' />
+					<img src={LinkedIn} alt='' />
+					<img src={Instagram} alt='' />
+				</div>
+			</div>
+			{/* right image side */}
+			<div className='i-right'>
+				<img src={Vector1} alt='' />
+				<img src={Vector2} alt='' />
+				<img src={boy} alt='' />
+				{/* animation */}
+				<motion.img
+					initial={{ left: "-36%" }}
+					whileInView={{ left: "-24%" }}
+					transition={transition}
+					src={glassesimoji}
+					alt=''
+				/>
 
-        <motion.div
-          initial={{ top: "-4%", left: "74%" }}
-          whileInView={{ left: "68%" }}
-          transition={transition}
-          className="floating-div"
-        >
-          <FloatinDiv img={crown} text1="Web" text2="Developer" />
-        </motion.div>
+				<motion.div
+					initial={{ top: "-4%", left: "74%" }}
+					whileInView={{ left: "68%" }}
+					transition={transition}
+					className='floating-div'
+				>
+					<FloatinDiv img={crown} text1='Software' text2='Engineer' />
+				</motion.div>
 
-        {/* animation */}
-        <motion.div
-          initial={{ left: "9rem", top: "18rem" }}
-          whileInView={{ left: "0rem" }}
-          transition={transition}
-          className="floating-div"
-        >
-          {/* floatinDiv mein change hy dark mode ka */}
-          <FloatinDiv img={thumbup} text1="Best Design" text2="Award" />
-        </motion.div>
+				{/* animation */}
+				<motion.div
+					initial={{ left: "9rem", top: "18rem" }}
+					whileInView={{ left: "0rem" }}
+					transition={transition}
+					className='floating-div'
+				>
+					<FloatinDiv
+						img={thumbup}
+						text1='Front-end Developer'
+						text2='Solution provider'
+					/>
+				</motion.div>
 
-        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
-        <div
-          className="blur"
-          style={{
-            background: "#C1F5FF",
-            top: "17rem",
-            width: "21rem",
-            height: "11rem",
-            left: "-9rem",
-          }}
-        ></div>
-      </div>
-    </div>
-  );
+				<div className='blur' style={{ background: "rgb(238 210 255)" }}></div>
+				<div
+					className='blur'
+					style={{
+						background: "#C1F5FF",
+						top: "17rem",
+						width: "21rem",
+						height: "11rem",
+						left: "-9rem",
+					}}
+				></div>
+			</div>
+		</div>
+	);
 };
 
 export default Intro;
