e394ffaf4b116794be254377ea830bc3c1736fcf
samerunet <samerunet@gmail.com>
2022-10-13 12:01:17 -0700

    emailjs added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import "./Footer.css";
 import Wave from "../../img/footer.png";
-import Insta from "@iconscout/react-unicons/icons/uil-instagram";
-import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
 import Gitub from "@iconscout/react-unicons/icons/uil-github";
+import Linkedin from "@iconscout/react-unicons/icons/uil-linkedin";
 
 const Footer = () => {
 	return (
 		<div className='footer'>
 			<img src={Wave} alt='' style={{ width: "100%" }} />
 			<div className='f-content'>
-				<span>samerunet@gmail.com</span>
+				<span className="text">samerunet@gmail.com</span>
 				<div className='f-icons'>
-					<Insta color='white' size={"3rem"} />
-					<Facebook color='white' size={"3rem"} />
-					<Gitub color='white' size={"3rem"} />
+					<Linkedin color='black' size={"3rem"} />
+					<Gitub color='black' size={"3rem"} />
 				</div>
 			</div>
 		</div>
